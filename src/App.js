d529c39d22819b2e5de17cd5cb50d58a7bf292c3
Taiwo Akindele <akindeletaiwo214@gmail.com>
2022-07-08 13:54:47 +0200

    orgs-list page created

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import { login } from './redux/features/user/userSlice'
 import ApiEndpoint from './pages/ApiEndpoint'
 import OrganizationPage from './pages/OrganizationPage'
 import { getSingleApis } from './redux/features/singleApi/singleApiSlice'
+import OrgList from './pages/OrgList'
 
 const useStyles = makeStyles({
   router_container: {
@@ -68,6 +69,7 @@ const App = () => {
           {/* Organization Pages */}
           <Route path='/orgs/:Id'  element={<OrganizationPage />} />
           <Route path='/orgs/create-new' element={<CreateOrg />} />
+          <Route path='/orgs-list' element={<OrgList />} />
         </Routes>
       </div>
     </ThemeProvider>
