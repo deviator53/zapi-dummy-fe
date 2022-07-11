5f2707d9a39c864edd195837bf4b354ed94e4e2b
Samson Okunola <smsnmicheal@gmail.com>
2022-07-11 02:26:36 +0100

    added form hook

---
+++ src/hooks/form-hook.js
@@ -0,0 +1,30 @@
+import { useReducer } from 'react'
+
+// Hook to handle form state
+const inputReducer = (state, action) => {
+    switch (action.type) {
+        case 'text':
+            return {
+                ...state,
+                [action.key]: action.value,
+            }
+            default:
+                return state
+            }
+        }
+
+
+// @param {Object} initialState - Initial state of the form
+export const useFormInputs = (initialState) => {
+    // useReducer is stores data better than useState
+    // The data persists even when the component is unmounted
+    const [inputs, dispatch] = useReducer(inputReducer, initialState)
+
+    // This function is used to update the state
+    // It takes in the key and value of the input
+    const handleInputChange = (e) => {
+        dispatch({type: 'text', key:[e.target.name], value: e.target.value})
+    }
+
+    return { inputs, handleInputChange }
+}
