f0597d908ff550a66d1ced1933db2b19583af360
Vinyl-Davyl <okononfuadavid@gmail.com>
2022-07-18 11:21:09 +0100

    Pricing page data match and SingleAPI page endpoints updated

--- src/components/Endpoints.jsx
+++ src/components/Endpoints.jsx
@@ -1,25 +1,53 @@
 import React from 'react'
 import { Stack, Typography } from '@mui/material'
 
+import { useSelector } from 'react-redux'
 import { InputField } from './index'
+import { useParams } from 'react-router';
+import { useFetch } from '../services/useFetch';
 
-const Endpoints = () => {
+const base_url = process.env.REACT_APP_BASE_URL
+
+
+const Endpoints = (props) => {
+  const { id } = useParams()
+
+  const { data } = useFetch(`${base_url}/endpoints/single-api/${id}`)
+  const { user } = useSelector(store => store.user)
+
+  const createData = (name, method ) => {
+      return { name, method }
+  }
+
+  const lists = []
+  data.map((endpoint) => {
+      if (endpoint.profileId === user.profileId) {
+          lists.push(createData(endpoint.name, endpoint.method))
+      }
+  })
+  
+  console.log(lists)
+
   return (
+    <>
     <Stack direction='column'>
       <Stack width='100%' height='72px' px={1} py={2} style={{ background: 'var(--grey)'}}>
         <InputField placeholder='Search...' />
       </Stack>
       <Stack direction='column' my={1}>
-        <Stack direction='row' spacing={1} alignItems='center'>
+      {lists.map((item) => (
+        <Stack  key={item.id} direction='row' spacing={1} alignItems='center'>
           <Typography variant='caption' color='success'>
-            GET
+          {item.method || 'GET' }
           </Typography>
           <Typography variant='body1'>
-            EndpointOne
+          {item.name|| 'endpoint one'}
           </Typography>
         </Stack>
+        ))}
       </Stack>
     </Stack>
+    </>
   )
 }
 
--- src/pages/SingleApi.jsx
+++ src/pages/SingleApi.jsx
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
-import { useSelector } from 'react-redux'
+import React, {useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom'
 import { Tab, Tabs, Stack, Typography } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 
+
 import logo from '../assets/LOGO (3).png'
 import { ApiMetrics, ApiDetails, Endpoints, EndpointSnippets, EndpointsParams, PopUp, ResizeableDiv, TabPanel, Pricing } from '../components'
+
+const base_url = process.env.REACT_APP_BASE_URL
 
 const useStyles = makeStyles({
   div:{
@@ -19,53 +22,76 @@ const useStyles = makeStyles({
   }
 })
 
+
 const SingleApi = () => {
+  
   const [tab, setTab]= useState(0);
   const classes = useStyles()
   const [openPopup, setOpenPopup] = useState(false)
+  const [openCarousel, setOpenCarousel] = useState([])
+  const { id } = useParams()
+  
+  try{
+    async function getCarousel() {
+        const res = await fetch(`${base_url}/api/${id}`)
+        const data = await res.json()
+        setOpenCarousel(data.data)
+        console.log(openCarousel)
+    }
+    useEffect(() => {
+        getCarousel()
+    }, [])
+  }catch (error){
+      
+  }
+
 
   return (
     <>
-    {openPopup && <PopUp closePopUp={() => setOpenPopup(false)} />}
-    <Stack direction='column'>
-      <Stack direction={{xs: 'column', sm: 'column', md: 'row', lg:'row'}} my={2} spacing={2} justifyContent='space-between' alignItems='center' >
-        <ApiDetails image={logo} name='Weather API' pricing='FREEMUIM' isVerified={false} author='zummit' lastUpdated='2days' category='Weather' featured='Popular APIs' />
-        <ApiMetrics popularity={9.9} latency={86} level={100}/>
-      </Stack>
-      <Tabs value={tab} onChange={(e, newValue)=>setTab(newValue)}>
-        <Tab label='Pricing'/>
-        <Tab label='Endpoints'/>
-        <Tab label='Tutorials' />
-      </Tabs>
-      <Stack direction={{xs: 'column', sm: 'column', md: 'row', lg: 'row'}} marginTop={2} spacing={2} alignItems='center'>
-        <Typography variant='h5'>Weather Map Api Documentation</Typography>
-      </Stack>
-      <Stack direction={{xs: 'column', sm: 'column', md: 'row', lg: 'row'}} spacing={2} my={1}alignItems='center'>
-        <Typography variant='subtitle1'>Get weather and weather forecasts for multiple cities.</Typography>
-      </Stack>
-      <Stack className={classes.tabs_container}>
-        <TabPanel value={tab} index={0}>
+          {openPopup && <PopUp closePopUp={() => setOpenPopup(false)} />}
           <Stack direction='column'>
-            <Pricing setOpenPopup={setOpenPopup} />
+            {/* {array.map(item => ( */}
+              <Stack>
+                <Stack direction={{ xs: 'column', sm: 'column', md: 'row', lg: 'row' }} my={2} spacing={2} justifyContent='space-between' alignItems='center' >
+                  <ApiDetails image={logo} name={openCarousel.name} pricing='FREEMUIM' isVerified={false} author='zummit' lastUpdated='2days' category={openCarousel.name} featured='Popular APIs' />
+                  <ApiMetrics popularity={9.9} latency={86} level={100} />
+                </Stack>
+                <Tabs value={tab} onChange={(e, newValue) => setTab(newValue)}>
+                  <Tab label='Pricing' />
+                  <Tab label='Endpoints' />
+                  <Tab label='Tutorials' />
+                </Tabs>
+                <Stack direction={{ xs: 'column', sm: 'column', md: 'row', lg: 'row' }} marginTop={2} spacing={2} alignItems='center'>
+                  <Typography variant='h5'>{openCarousel.name} Api Documentation</Typography>
+                </Stack>
+                <Stack direction={{ xs: 'column', sm: 'column', md: 'row', lg: 'row' }} spacing={2} my={1} alignItems='center'>
+                  <Typography variant='subtitle1'>{openCarousel.description}</Typography>
+                </Stack>
+                <Stack className={classes.tabs_container}>
+                  <TabPanel value={tab} index={0}>
+                    <Stack direction='column'>
+                      <Pricing setOpenPopup={setOpenPopup} />
+                    </Stack>
+                  </TabPanel>
+                  <TabPanel value={tab} index={1}>
+                    <Stack direction='row'>
+                      <ResizeableDiv>
+                        <Endpoints />
+                      </ResizeableDiv>
+                      <ResizeableDiv>
+                        <EndpointsParams />
+                      </ResizeableDiv>
+                      <Stack direction='column' className={classes.div}>
+                        <EndpointSnippets />
+                      </Stack>
+                    </Stack>
+                  </TabPanel>
+                  <TabPanel value={tab} index={2}>Tutorial</TabPanel>
+                </Stack>
+              </Stack>
+            {/* ))} */}
           </Stack>
-        </TabPanel>
-        <TabPanel value={tab} index={1}>
-          <Stack direction='row'>
-            <ResizeableDiv>
-              <Endpoints />
-            </ResizeableDiv>
-            <ResizeableDiv>
-              <EndpointsParams />
-            </ResizeableDiv>
-            <Stack direction='column' className={classes.div}>
-              <EndpointSnippets />
-            </Stack>
-          </Stack>
-        </TabPanel>
-        <TabPanel value={tab} index={2}>Tutorial</TabPanel>
-      </Stack>
-    </Stack>
-    </>
+        </>
   )
 }
 
