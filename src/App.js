55214ee3b363ad4da25778a41f0cb791a10b4143
Letam Davidson <davidsonletam@gmail.com>
2022-07-27 18:03:39 +0100

    Security page was created

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ import { ThemeProvider } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Cookies from "universal-cookie";
 
-import { ForgotPassword, Home, LoginPage, SingleApi, UserProfile, Categories, Category, CreateOrg, Signup, Settings, MyApiPage, Endpoint, EmailAutoVerify, ErrorPage } from './pages'
-import EmailVerify from './pages/EmailVerify';
-import PasswordReset from './pages/PasswordReset'
-import { Navbar } from './components'
-import { theme } from './theme'
-import { getApis } from './redux/features/api/apiSlice'
-import { getSingleApis } from './redux/features/singleApi/singleApiSlice'
-import { login } from './redux/features/user/userSlice'
-import ApiEndpoint from './pages/ApiEndpoint'
-import OrganizationPage from './pages/OrganizationPage'
-import OrgList from './pages/OrgList'
-import { RequireAuth } from './components/RequireAuth'
+import {
+	ForgotPassword,
+	Home,
+	LoginPage,
+	SingleApi,
+	UserProfile,
+	Categories,
+	Category,
+	CreateOrg,
+	Signup,
+	Settings,
+	MyApiPage,
+	Endpoint,
+	EmailAutoVerify,
+	ErrorPage,
+} from "./pages";
+import EmailVerify from "./pages/EmailVerify";
+import PasswordReset from "./pages/PasswordReset";
+import { Navbar } from "./components";
+import { theme } from "./theme";
+import { getApis } from "./redux/features/api/apiSlice";
+import { getSingleApis } from "./redux/features/singleApi/singleApiSlice";
+import { login } from "./redux/features/user/userSlice";
+import ApiEndpoint from "./pages/ApiEndpoint";
+import OrganizationPage from "./pages/OrganizationPage";
+import OrgList from "./pages/OrgList";
+import { RequireAuth } from "./components/RequireAuth";
+import SecurityMyApi from "./pages/SecurityMyApi";
 
 const useStyles = makeStyles({
 	router_container: {
@@ -63,49 +79,55 @@ const App = () => {
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Navbar query={query} setQuery={setQuery} />
-      <div className={classes.router_container}>
-        <Routes>
-          {/* General Pages */}
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/forgot-password' element={<ForgotPassword />} />
-          <Route path='/email-verify' element={<EmailVerify />} />
-          <Route path='/email-verification/:token' element={<EmailAutoVerify />} />
-          <Route path='/password-reset/:id/:token' element={<PasswordReset />} />
+	return (
+		<ThemeProvider theme={theme}>
+			<Navbar query={query} setQuery={setQuery} />
+			<div className={classes.router_container}>
+				<Routes>
+					{/* General Pages */}
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/signup" element={<Signup />} />
+					<Route path="/forgot-password" element={<ForgotPassword />} />
+					<Route path="/email-verify" element={<EmailVerify />} />
+					<Route
+						path="/email-verification/:token"
+						element={<EmailAutoVerify />}
+					/>
+					<Route
+						path="/password-reset/:id/:token"
+						element={<PasswordReset />}
+					/>
 
-			{/* API Pages */}
-			<Route path="/api/:id" element={<SingleApi />} />
-			<Route path="/api/categories" element={<Categories />} />
-			<Route path="/api/categories/:id" element={<Category />} />
+					{/* API Pages */}
+					<Route path="/api/:id" element={<SingleApi />} />
+					<Route path="/api/categories" element={<Categories />} />
+					<Route path="/api/categories/:id" element={<Category />} />
 
-			<Route element={<RequireAuth />}>
-				<Route path="/api/api/new/:id" element={<MyApiPage />} />
-				<Route path="/api/endpoint/new/:id" element={<ApiEndpoint />} />
-				<Route path="/api/endpoints/:id" element={<Endpoint />} />
+					<Route element={<RequireAuth />}>
+						<Route path="/api/api/new/:id" element={<MyApiPage />} />
+						<Route path="/api/endpoint/new/:id" element={<ApiEndpoint />} />
+						<Route path="/api/endpoints/:id" element={<Endpoint />} />
 
-				<Route path='/api/api/new/:id' element={<MyApiPage />} />
-				<Route path='/api/endpoint/new/:id' element={<ApiEndpoint />} />
-				<Route path='/api/endpoints/:id' element={<Endpoint />} />
+						<Route path="/api/api/new/:id" element={<MyApiPage />} />
+						<Route path="/api/endpoint/new/:id" element={<ApiEndpoint />} />
+						<Route path="/api/endpoints/:id" element={<Endpoint />} />
+						<Route path="/api/security/:id" element={<SecurityMyApi />} />
 
-				{/* User Pages */}
-				<Route path='/user/:id' element={<UserProfile />} />
-				<Route path='/user/settings' element={<Settings />} />
+						{/* User Pages */}
+						<Route path="/user/:id" element={<UserProfile />} />
+						<Route path="/user/settings" element={<Settings />} />
 
-				{/* Organization Pages */}
-				<Route path='/orgs/:Id'  element={<OrganizationPage />} />
-				<Route path='/orgs/create-new' element={<CreateOrg />} />
-				<Route path='/orgs-list/:id' element={<OrgList />} />
-			
-			</Route>
-			<Route path='/error' element={<ErrorPage />} />
-			</Routes>
-      </div>
-    </ThemeProvider>
-  )
-}
+						{/* Organization Pages */}
+						<Route path="/orgs/:Id" element={<OrganizationPage />} />
+						<Route path="/orgs/create-new" element={<CreateOrg />} />
+						<Route path="/orgs-list/:id" element={<OrgList />} />
+					</Route>
+					<Route path="/error" element={<ErrorPage />} />
+				</Routes>
+			</div>
+		</ThemeProvider>
+	);
+};
 
-export default App
+export default App;
